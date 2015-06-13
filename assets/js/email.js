91f642772498b4b8f7cd2a1d11f19c9f4e771513
Jack Song <133342301@163.com>
2015-06-14 02:50:24 +1000

    Implemented event tracking

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,4 +1,86 @@
-function submitForm(form) {
+//Google Analytics Events tracking & email sending
+// Using jQuery Event API v1.3
+$('#signUpTop').on('click', function() {
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'Sign up',
+		'eventValue': 0
+	});
+});
+$('#firstButton').on('click', function() {
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'More info buttons',
+		'eventValue': 0
+	});
+});
+$('#secondButton').on('click', function() {
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'More info buttons',
+		'eventValue': 1
+	});
+});
+$('#thirdButton').on('click', function() {
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'More info buttons',
+		'eventValue': 2
+	});
+});
+$('#forthButton').on('click', function() {
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'More info buttons',
+		'eventValue': 3
+	});
+});
+$('#fifthButton').on('click', function() {
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'More info buttons',
+		'eventValue': 4
+	});
+});
+$('#sixthButton').on('click', function() {
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'More info buttons',
+		'eventValue': 5
+	});
+});
+$('#seventhButton').on('click', function() {
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'More info buttons',
+		'eventValue': 6
+	});
+});
+$('#eighthButton').on('click', function() {
+	//Register with Google
+	ga('send', {
+		'hitType': 'event', // Required.
+		'eventCategory': 'button', // Required.
+		'eventAction': 'click', // Required.
+		'eventLabel': 'Sign up',
+		'eventValue': 1
+	});
 	//Validate email
 	var validator = function validateEmail(email) {
 		var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
@@ -30,4 +112,4 @@ function submitForm(form) {
 			}
 		});
 	}
-}
+});

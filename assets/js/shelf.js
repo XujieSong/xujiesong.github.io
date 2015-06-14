c867dd2d761f23c3f5742fed3f4027d229dacbe4
Jack Song <133342301@163.com>
2015-06-14 15:16:36 +1000

    Added event listener for page

--- assets/js/shelf.js
+++ assets/js/shelf.js
@@ -1,3 +1,107 @@
+$(document).ready(function() {
+	console.log('ready');
+
+	var switch0 = false;
+	var switch1 = false;
+	var switch2 = false;
+	var switch3 = false;
+	var switch4 = false;
+	var switch5 = false;
+	var switch6 = false;
+	var switch7 = false;
+	var section0 = 0;
+	var section1 = $('#banner').innerHeight();
+	var section2 = section1 + $('#one').innerHeight();
+	var section3 = section2 + $('#two').innerHeight();
+	var section4 = section3 + $('#three').innerHeight();
+	var section5 = section4 + $('#four').innerHeight();
+	var section6 = section5 + $('#footer').innerHeight();
+
+	$(window).scroll(function() {
+		var scrollTop = $(window).scrollTop();
+		if (switch0 == false) {
+			ga('send', {
+				'hitType': 'event', // Required.
+				'eventCategory': 'View', // Required.
+				'eventAction': 'scroll', // Required.
+				'eventLabel': 'Banner viewed',
+				'eventValue': 0
+			});
+			console.log("Banner viewed");
+			switch0 = true;
+		} else if (section0 < scrollTop && switch1 == false) {
+			ga('send', {
+				'hitType': 'event', // Required.
+				'eventCategory': 'View', // Required.
+				'eventAction': 'scroll', // Required.
+				'eventLabel': 'Intro viewed',
+				'eventValue': 0
+			});
+			console.log("section 1 viewed");
+			switch1 = true;
+		} else if (section1 < scrollTop && switch2 == false) {
+			ga('send', {
+				'hitType': 'event', // Required.
+				'eventCategory': 'View', // Required.
+				'eventAction': 'scroll', // Required.
+				'eventLabel': 'Design viewed',
+				'eventValue': 0
+			});
+			console.log("section 2 viewed");
+			switch2 = true;
+		} else if (section2 < scrollTop && switch3 == false) {
+			ga('send', {
+				'hitType': 'event', // Required.
+				'eventCategory': 'View', // Required.
+				'eventAction': 'scroll', // Required.
+				'eventLabel': 'Marketing viewed',
+				'eventValue': 0
+			});
+			console.log("section 3 viewed");
+			switch3 = true;
+		} else if (section3 < scrollTop && switch4 == false) {
+			ga('send', {
+				'hitType': 'event', // Required.
+				'eventCategory': 'View', // Required.
+				'eventAction': 'scroll', // Required.
+				'eventLabel': 'Customer viewed',
+				'eventValue': 0
+			});
+			console.log("section 4 viewed");
+			switch4 = true;
+		} else if (section4 < scrollTop && switch5 == false) {
+			ga('send', {
+				'hitType': 'event', // Required.
+				'eventCategory': 'View', // Required.
+				'eventAction': 'scroll', // Required.
+				'eventLabel': 'Features viewed',
+				'eventValue': 0
+			});
+			console.log("section 5 viewed");
+			switch5 = true;
+		} else if (section5 < scrollTop && switch6 == false) {
+			ga('send', {
+				'hitType': 'event', // Required.
+				'eventCategory': 'View', // Required.
+				'eventAction': 'scroll', // Required.
+				'eventLabel': 'Free Trail viewed',
+				'eventValue': 0
+			});
+			console.log("section 6 viewed");
+			switch6 = true;
+		} else if (section6 < scrollTop && switch7 == false) {
+			ga('send', {
+				'hitType': 'event', // Required.
+				'eventCategory': 'View', // Required.
+				'eventAction': 'scroll', // Required.
+				'eventLabel': 'Contact viewed',
+				'eventValue': 0
+			});
+			console.log("section 7 viewed");
+			switch7 = true;
+		}
+	});
+});
 //Sending email
 function submitForm(form) {
 	//Validate email
@@ -33,13 +137,20 @@ function submitForm(form) {
 	}
 }
 //Initialize Google Analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o),
+		m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-  ga('create', 'UA-58021961-1', 'auto');
-  ga('send', 'pageview');
+ga('create', 'UA-58021961-1', 'auto');
+ga('send', 'pageview');
 // Using jQuery Event API v1.3
 $('#signUpTop').on('click', function() {
 	ga('send', {
